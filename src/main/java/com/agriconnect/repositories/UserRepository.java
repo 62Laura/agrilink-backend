00a079d43e6b4d0c348cfd2ed2a9cefe19da60ad
65laura <ishimwelauraceline@gmail.com>
2025-07-14 16:33:08 +0200

    ft:membership application process and approval

--- src/main/java/com/agriconnect/repositories/UserRepository.java
+++ src/main/java/com/agriconnect/repositories/UserRepository.java
@@ -7,13 +7,10 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
-    Optional<User> findByUsername(String username);
     boolean existsByEmail(String email);
-    boolean existsByUsername(String username);
     List<User> findByUserType(UserType userType);
     List<User> findByUserTypeAndIsApproved(UserType userType, boolean isApproved);
 
