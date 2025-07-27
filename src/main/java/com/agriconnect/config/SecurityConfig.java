c4314e7f50719673ba8be12586669e63079dd86f
65laura <ishimwelauraceline@gmail.com>
2025-07-27 20:18:06 +0200

    ft: fix cors

--- src/main/java/com/agriconnect/config/SecurityConfig.java
+++ src/main/java/com/agriconnect/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import com.agriconnect.services.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -51,6 +53,7 @@ public class SecurityConfig {
 
 
     @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("https://agri-linkk.netlify.app"));
